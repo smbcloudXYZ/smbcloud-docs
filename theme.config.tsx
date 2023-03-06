06d2a69ae49adc1e0ebb6e6730eb1111f4ba87ab
Seto Elkahfi <hej@setoelkahfi.se>
2023-03-06 14:26:46 +0100

    First release

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,13 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>SmbPndk Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/smbpndk/smbpndk-cli',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/smbpndk/smbpndk-docs/tree/main',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'SmbPndk',
   },
 }
 
