888fe6ea56a9aa0171367e3a8540ef8ac89e2ae6
Seto Elkahfi <hej@setoelkahfi.se>
2024-12-31 13:49:04 +0200

    update docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,15 +1,30 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
   logo: <span>smbCloud Documentation</span>,
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta name="description" content="smbCloud Documentation" />
+      <meta name="og:title" content="smbCloud Documentation" />
+      <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
+    </>
+  ),
   project: {
-    link: 'https://github.com/smbcloudXYZ/smbcloud-cli',
+    link: "https://github.com/smbcloudXYZ",
   },
-  docsRepositoryBase: 'https://github.com/smbcloudXYZ/smbcloud-docs/tree/main',
+  docsRepositoryBase: "https://github.com/smbcloudXYZ/smbcloud-docs/tree/main",
   footer: {
-    text: 'smbCloud',
+    content: (
+      <span>
+        MIT {new Date().getFullYear()} ©{' '}
+        <a href="https://docs.smbcloud.xyz" target="_blank">
+          smbCloud Documentation
+        </a>
+      </span>
+    )
   },
-}
+};
 
-export default config
+export default config;
